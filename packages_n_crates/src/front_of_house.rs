2197994dbfe4de67c29c24949e8d264183ba7216
greyland miller <greyland.official@gmail.com>
2024-05-01 22:49:07 -0700

    generi types are cool

--- packages_n_crates/src/front_of_house.rs
+++ packages_n_crates/src/front_of_house.rs
@@ -1,5 +1,3 @@
-mod front_of_house {
-    pub fn add_to_waitlist() {
-        println!("Adding customer to waitlist")
-    }
+pub mod hosting {
+    pub fn add_to_waitlist() {}
 }
--- packages_n_crates/src/main.rs
+++ packages_n_crates/src/main.rs
@@ -1,23 +1,27 @@
 //:use crate::garden::vegetables::Asparagus;
 
 //pub mod garden;
+//
+// pub mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {
+//             println!("Adding customer to waitlist")
+//         }
+//     }
+// }
+//
+// use crate::front_of_house::hosting;
 
-pub mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {
-            println!("Adding customer to waitlist")
-        }
-    }
-}
+// fn eat_at_restruant() {
+//     hosting::add_to_waitlist();
+// }
+//
 
-use crate::front_of_house::hosting;
-
-fn eat_at_restruant() {
-    hosting::add_to_waitlist();
-}
+use crate::eat_at_restruant;
 
 fn main() {
     //let plant = Asparagus {};
     //println!("I'm growing a {}", plant);
-    eat_at_restruant();
-} 
+    /* eat_at_restruant */
+    ();
+}
